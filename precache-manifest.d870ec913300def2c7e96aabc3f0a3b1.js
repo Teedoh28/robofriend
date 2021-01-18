ae9701f9b188aa215b7816e86ad903242f1da880
thsandiso <Tando.Maliwa@metropolitan.co.za>
2021-01-18 19:37:31 +0200

    Updates

---
+++ precache-manifest.d870ec913300def2c7e96aabc3f0a3b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b13f1cde082637aa6be24572b2e8b289",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "ddc3430fba88b89981f6",
+    "url": "/robofriend/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "adde33dcbf45a3edd0c8",
+    "url": "/robofriend/static/css/main.b722fd8e.chunk.css"
+  },
+  {
+    "revision": "ddc3430fba88b89981f6",
+    "url": "/robofriend/static/js/2.22daa22d.chunk.js"
+  },
+  {
+    "revision": "adde33dcbf45a3edd0c8",
+    "url": "/robofriend/static/js/main.ed513c97.chunk.js"
+  },
+  {
+    "revision": "eee6a9cd989deb0bd4d8",
+    "url": "/robofriend/static/js/runtime~main.2fb9822a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
